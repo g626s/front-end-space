ef142abd0a4532306b0411d26de79393fcb49d0e
Gus Sanchez <gustavosypr@gmail.com>
2025-01-16 16:11:30 -0800

    Updates

--- my-react-app/src/components/Contact.jsx
+++
@@ -1,9 +0,0 @@
-export default function Contact() {
-    return (
-        <main>
-            <section id="current-status" aria-labelledby="notice-for-page" className="notice container">
-                <h3 id="notice-for-page">Coming soon!</h3>
-            </section>
-        </main>
-    );
-}
--- my-react-app/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <section className="footer-section container">
-            <footer className="footer-copyright">&copy;2024-2024 Gestalt Studios - Designed & Built by Gustavo Sanchez 2024</footer>
-        </section>
-    )
-};
